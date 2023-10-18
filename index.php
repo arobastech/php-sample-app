1875cf650c04b00f5a4763a73766aea1daeca271
arobastech <93337628+arobastech@users.noreply.github.com>
2023-10-18 05:37:48 +0100

    UPDATE 18/10/23

--- index.php
+++
@@ -1,9 +0,0 @@
-<?php 
-require('vendor/autoload.php');
-\PHP_Timer::start();
-sleep(rand(1, 3));
-$time = \PHP_Timer::stop();
-print \PHP_Timer::secondsToTimeString($time);
-
-echo "<br /><br />";
-phpinfo();
